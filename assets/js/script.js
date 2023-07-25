ca38d1e4bd2a06fd0c614802dd1e68888884e96a
London Jensen <londonjensen@Londons-MacBook-Pro.local>
2023-07-24 17:38:08 -0700

    401 error is resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
 const apiKey = "fe27a4ac1f7e47c2d9d5b18ee79cc084";
 // const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?=&units=metric&q=";
 // I am getting an error; also need to delete my api key from the url below
-const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=benicia,CA,840&appid=fe27a4ac1f7e47c2d9d5b18ee79cc084"
+
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=fe27a4ac1f7e47c2d9d5b18ee79cc084&units=metric"
+    const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=fe27a4ac1f7e47c2d9d5b18ee79cc084&units=metric"
+    const response = await fetch(apiUrl);
 
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
+        console.log(data)
 
         document.querySelector(".city").innerHTML = data.name;
         // saying that temp is not defined
@@ -41,10 +44,35 @@ async function checkWeather(city) {
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
     }
+    const forecastResponse = await fetch(forecastUrl);
+
+    if (forecastResponse.status == 404) {
+        // document.querySelector(".error").style.display = "block";
+        // document.querySelector(".weather").style.display = "none";
+        console.log("404error")
+    } else {
+        var forecast = await forecastResponse.json();
+        console.log(forecast)
+        for (let i = 0; i < forecast.list.length; i++) {
+            if (forecast.list[i].dt_txt.includes("12:00:00")){
+                console.log(forecast.list[i])
+            }
+          }
+    }
 }
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
+    var history = JSON.parse(localStorage.getItem("previousSearch")) || []
+    if (!history.includes(searchBox.value)){
+        history.push(searchBox.value)
+        localStorage.setItem("previousSearch", JSON.stringify(history))
+    }
 })
 
-// TODO need to add this into local storage.
+
+$(".search").each(function(){
+    var id = $(this).attr("id")
+    $(this).children("input").val(localStorage.getItem(id))
+})
+// TODO need to add this into local storage. There needs to be a section for the search history
